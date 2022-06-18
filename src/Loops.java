19637e64d327e06bdf3d4ae74cbba2bc65c81c7c
MichaelAmangelo <85372003+MichaelAmangelo@users.noreply.github.com>
2022-06-18 16:05:50 -0400

    Update Loops.java

--- src/Loops.java
+++ src/Loops.java
@@ -2,11 +2,13 @@
 import java.util.Scanner;
 
 public class Loops {
+    
     public static void main(String[] args) {
 
         Scanner l = new Scanner(System.in);
 
         System.out.println("Enter an integer greater than 1: ");
+        
         int n = l.nextInt();
 
         int count = 1;
@@ -16,11 +18,14 @@ public class Loops {
         while (count <= n) {
 
             if (n % count == 0) {
+                
                 sum += count;
-
+                
             }
+            
             count++;
         }
+        
         System.out.println( "The sum of all factors of " + n +  " is " + sum );
     }
 }
